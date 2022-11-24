bfe5a0c5f7a2113e4d781c0182c2c352745a1a21
Rafael Pszenny <onliniak@gmail.com>
2022-11-24 18:25:04 +0000

    fix: Favorited endpoint return placeholder

--- src/Controller/Articles/ArticlesFavoriteController.php
+++ src/Controller/Articles/ArticlesFavoriteController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Articles;
 
+use App\Repository\ArticlesRepository;
 use App\Repository\FavoritedRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,22 +13,17 @@ use Symfony\Component\Security\Core\Security;
 class ArticlesFavoriteController extends AbstractController
 {
     #[Route('/api/articles/{slug}/favorite', name: 'app_articles_favorite', methods: ['POST'])]
-    public function create(FavoritedRepository $favoritedRepository, Security $security, string $slug): Response
+    public function create(FavoritedRepository $favoritedRepository, Security $security, string $slug, 
+                           ArticlesRepository $articlesRepository): Response
     {
         $favoritedRepository->favorite($slug, $security->getUser()->getUserIdentifier());
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ArticlesFavoriteController.php',
-        ]);
+        return $this->json($articlesRepository->getSingleArticle($slug));
     }
     #[Route('/api/articles/{slug}/favorite', name: 'app_articles_unfavorite', methods: ['DELETE'])]
-    public function delete(FavoritedRepository $favoritedRepository, Security $security, string $slug): Response
+    public function delete(FavoritedRepository $favoritedRepository, Security $security, string $slug,
+                           ArticlesRepository $articlesRepository): Response
     {
-
         $favoritedRepository->unfavorite($slug, $security->getUser()->getUserIdentifier());
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ArticlesFavoriteController.php',
-        ]);
+        return $this->json($articlesRepository->getSingleArticle($slug));
     }
 }
